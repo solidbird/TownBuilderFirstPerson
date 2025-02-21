fe244d2ca05326040006232bc517e5277eab4e1e
Serdar Akcay <serdar.akcay@alumni.fh-aachen.de>
2025-02-21 23:40:33 +0100

    Init makefile and main.c

---
+++ src/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <raylib.h>
+
+int main(int argc, char **argv){
+	InitWindow(800, 600, "WaveFunctionColapse Test");
+
+	while(!WindowShouldClose()){
+		
+	}
+	return 0;
+}
