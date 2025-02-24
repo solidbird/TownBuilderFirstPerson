ee0812fa5c563b2f19b0480dfe2ae52c54715c06
Serdar Akcay <serdar.akcay@alumni.fh-aachen.de>
2025-02-24 02:43:06 +0100

    main.c: functioning "put block into place" with ghost block

--- src/main.c
+++ src/main.c
@@ -12,6 +12,13 @@
 
 #define CUBE_SIZE 0.5
 
+Vector3 get_camera_look_direction(Camera3D *cam3d){
+	Vector3 camera_direction = Vector3Subtract(cam3d->target, cam3d->position);
+	camera_direction = Vector3Normalize(camera_direction);
+
+	return camera_direction;
+}
+
 int main(int argc, char **argv){
 	InitWindow(1600, 900, "WaveFunctionColapse Test");
 	DisableCursor();
@@ -23,6 +30,15 @@ int main(int argc, char **argv){
 	cam3d.fovy = 60.0;
 	cam3d.projection = CAMERA_PERSPECTIVE;
 
+
+	Ray camera_ray = {
+		cam3d.position,
+		get_camera_look_direction(&cam3d)
+	};
+
+	Vector3 *block_positions = (Vector3*) malloc(sizeof(Vector3) * 500);
+	int block_pos_index = 0;
+
 	SetTargetFPS(60);
 
 	while(!WindowShouldClose()){
@@ -30,14 +46,31 @@ int main(int argc, char **argv){
 		ClearBackground(BLACK);
 		BeginMode3D(cam3d);
 		UpdateCamera(&cam3d, CAMERA_FREE);
-		TraceLog(LOG_INFO, "(%f, %f, %f)", cam3d.position.x, cam3d.position.y, cam3d.position.z);
 
-		for(int i = 0; i < 10; i++){
+		/*for(int i = 0; i < 10; i++){
 			DrawCubeV((Vector3){0, CUBE_SIZE * i, 0}, (Vector3){CUBE_SIZE, CUBE_SIZE, CUBE_SIZE}, RED);
+		}*/
+
+		camera_ray.position = cam3d.position;
+		camera_ray.direction = get_camera_look_direction(&cam3d);
+
+		RayCollision rc = GetRayCollisionQuad(camera_ray, 
+			(Vector3){10, 0, 10},
+			(Vector3){10, 0, -10},
+			(Vector3){-10, 0, -10},
+			(Vector3){-10, 0, 10});
+		TraceLog(LOG_INFO, "%d, (%f, %f, %f)",block_pos_index, rc.point.x, rc.point.y, rc.point.z);
+		if(rc.hit){
+			if(IsKeyPressed(KEY_I)) block_positions[block_pos_index++] = rc.point; 
+			DrawCubeV(rc.point, (Vector3){CUBE_SIZE*0.5, CUBE_SIZE*0.5, CUBE_SIZE*0.5}, GRAY);
+		}
+
+		for(int i = 0; i < block_pos_index; i++){
+			DrawCubeV(block_positions[i], (Vector3){CUBE_SIZE*0.5, CUBE_SIZE*0.5, CUBE_SIZE*0.5}, RED);
 		}
 		
-		DrawPlane((Vector3){0,0,0}, (Vector2){10000, 10000}, BLUE);
-		DrawGrid(10000, CUBE_SIZE * 4);
+		DrawPlane((Vector3){0,0,0}, (Vector2){1000, 1000}, BLUE);
+		DrawGrid(1000, CUBE_SIZE * 4);
 		EndMode3D();
 
 		DrawFPS(10, 10);
